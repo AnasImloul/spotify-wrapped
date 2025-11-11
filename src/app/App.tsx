ada16e23f7262d3e738a64f0df805cbd9551903a
AnasImloul <anas.imloul27@gmail.com>
2025-11-11 20:01:55 +0100

    Update Vercel Analytics location

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,11 +6,13 @@
 import { AppProviders } from './providers/AppProviders';
 import AppRoutes from './router/AppRoutes';
 import { PWAInstallPrompt } from '@/features/onboarding/components/PWAInstallPrompt';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
     <>
       <PWAInstallPrompt />
+      <Analytics />
       <AppProviders>
         <AppRoutes />
       </AppProviders>
